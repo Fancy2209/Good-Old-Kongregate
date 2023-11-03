adc61ce04be5259a65c8e334898444bc769c8562
Paulo Medeiros <paulo.medeiros1302@gmail.com>
2023-11-03 21:03:52 -0100

    Forgot to commit this

--- JS/KOG.user.js
+++ JS/KOG.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Good Old Kongregate
 // @namespace    http://tampermonkey.net/
-// @version      0.69
+// @version      0.69.1
 // @description  Gone but not forgotten
 // @author       Fancy2209, Matrix4348
 // @match         *://www.kongregate.com/*
