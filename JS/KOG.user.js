8cb74597bd5535bfaef00f46534de4d4acd79f57
Matrix4348 <127596209+Matrix4348@users.noreply.github.com>
2023-12-09 11:34:31 +0100

    Fix css loading issues
    In some cases, css were not loading. This was probably caused by some kind of race between the loading of the page and the loading of the user script or user script manager: the mutation observer would only be added AFTER the css were loaded. These changes fix that.
    Additionnally, I put the functions outside of the big one (for aesthetic reasons, I guess...).

--- JS/KOG.user.js
+++ JS/KOG.user.js
@@ -1,7 +1,7 @@
 // ==UserScript==
 // @name         Good Old Kongregate
 // @namespace    http://tampermonkey.net/
-// @version      0.71
+// @version      0.72
 // @description  Gone but not forgotten
 // @author       Fancy2209, Matrix4348
 // @match         *://www.kongregate.com/*
@@ -11,14 +11,140 @@
 // @require      https://code.jquery.com/jquery-1.9.1.js
 // ==/UserScript==
 
+function TimeToLogin(){
+    function updatePlaylist() {
+        var e = active_user.playLatersCount();
+        $$(".play-laters-count-link").each(function (t) {
+            (t.title = e + " games in your playlist."), t.down(".play-laters-count").update(e);
+        });
+    }
+    function updateFavorites() {
+        var e = active_user.favoritesCount();
+        $$(".favorites-count-link").each(function (t) {
+            (t.title = e + " favorites."), t.down(".favorites-count").update(e);
+        });
+    }
+    function updateFriendInfo(e) {
+        e.select(".friends_online_count").each(function (e) {
+            e.update(active_user.friendsOnlineCount());
+        }),
+            active_user.friendsOnlineCount() > 0
+            ? e.select(".friends_online_link").each(function (e) {
+            e.title = active_user.friendsOnlineNames();
+        })
+        : e.select(".friends_online_link").each(function (e) {
+            e.title = "No friends online.";
+        });
+    }
+
+    var go=0, t = $("welcome");
+    if(typeof(active_user)!="undefined"){
+        if (active_user.isAuthenticated() && t!=null){ go=1; }
+    }
+    if(go){
+        var e = active_user.getAttributes();
+        updateFriendInfo(t), updatePlaylist(), updateFavorites();
+        var n = new Element("img").writeAttribute({ id: "welcome_box_small_user_avatar", src: e.avatar_url, title: e.username, name: "user_avatar", alt: "Avatar for " + e.username, height: 28, width: 28 });
+        t.down("span#small_avatar_placeholder").update(n),
+            t.select(".facebook_nav_item").each(function (e) { active_user.isFacebookConnected() && e.hide(); }),
+            $("mini-profile-level").writeAttribute({ class: "spritesite levelbug level_" + e.level, title: "Level " + e.level }),
+            active_user.populateUserSpecificLinks(t),
+            t.select(".username_holder").each(function (e) { e.update(active_user.username()); });
+        var a = active_user.unreadShoutsCount() + active_user.unreadWhispersCount() + active_user.unreadGameMessagesCount();
+        a > 0 &&
+            ($("profile_bar_messages").addClassName("alert_messages"),
+             $("profile_control_unread_message_count").update(a),
+             $("profile_control_unread_message_count").addClassName("mls has_messages"),
+             $("my-messages-link").setAttribute("title", active_user.unreadShoutsCount() + " shouts, " + active_user.unreadWhispersCount() + " whispers"),
+             0 !== active_user.unreadWhispersCount()
+             ? $("my-messages-link").setAttribute("href", "/accounts/" + active_user.username() + "/private_messages")
+             : 0 !== active_user.unreadGameMessagesCount() && $("my-messages-link").setAttribute("href", "/accounts/" + active_user.username() + "/game_messages")),
+            null !== active_user.chipsBalance() , ($("blocks_balance").update(active_user.chipsBalance()), $("blocks").show()),
+            $("guest_user_welcome_content").hide(),
+            $("nav_welcome_box").show();
+    }
+    else{ setTimeout(function(){ TimeToLogin(); },10000); }
+}
+
+function ReopenChat(A){
+    var go=0;
+    if(typeof(holodeck)!="undefined" && document.getElementById("chat_tab")!=null){
+        if(holodeck.ready){ go=1; }
+    }
+    if(go){ document.getElementById("chat_tab").style.display="";}
+    else if(A){ setTimeout(function(B){ ReopenChat(B); },1000, A-1); }
+}
+
+function fill_games_tab(A){
+    if(typeof(navigationData)!="undefined"){
+        if(navigationData.user.authenticated){
+            document.getElementById("GOK_recently_played").setAttribute("href",navigationData.user.recently_played_path);
+            var l=navigationData.games.recently_played.length;
+            if(l>0){
+                document.getElementById("GOK_no_recently_played_game").style.display="none";
+                document.getElementById("GOK_recently_played_1").style.display="";
+                document.getElementById("GOK1_image").setAttribute("src",navigationData.games.recently_played[0].icon_path);
+                document.getElementById("GOK1_name").innerText=navigationData.games.recently_played[0].title;
+                document.getElementById("GOK_recently_played_1").setAttribute("href",navigationData.games.recently_played[0].game_path);
+                for(let k=1; k<l; k++){
+                    document.getElementById("GOK_recently_played_"+(k+1)).setAttribute("href",navigationData.games.recently_played[k].game_path);
+                    document.getElementById("GOK_recently_played_"+(k+1)).innerText=navigationData.games.recently_played[k].title;
+                    document.getElementById("GOK_recently_played_"+(k+1)).style.display="";
+                }
+            }
+            document.getElementById("GOK_playlist").setAttribute("href",navigationData.user.playlist_path);
+            var p=navigationData.games.playlist.length;
+            if(p>0){
+                document.getElementById("GOK_no_playlist").style.display="none";
+                document.getElementById("GOK_playlist_1").style.display="";
+                document.getElementById("GOK_playlist_1_image").setAttribute("src",navigationData.games.playlist[0].icon_path);
+                document.getElementById("GOK_playlist_1_name").innerText=navigationData.games.playlist[0].title;
+                document.getElementById("GOK_playlist_1").setAttribute("href",navigationData.games.playlist[0].game_path);
+                for(let k=1; k<p; k++){
+                    document.getElementById("GOK_playlist_"+(k+1)).setAttribute("href",navigationData.games.playlist[k].game_path);
+                    document.getElementById("GOK_playlist_"+(k+1)).innerText=navigationData.games.playlist[k].title;
+                    document.getElementById("GOK_playlist_"+(k+1)).style.display="";
+                }
+            }
+            else{ document.getElementById("GOK_no_playlist_text").innerText="Add games to play them later."; }
+        }
+    }
+    else if(A){ setTimeout(function(B){ fill_games_tab(B); },1000, A-1); }
+}
+
+function replace_css(){
+    var N=document.head.getElementsBySelector('link[rel="stylesheet"]');
+    for(let n of N){
+        if(n.href.search("gamepage_merged")>-1 && n.getAttribute("data-turbo-track")=="reload"){
+            n.remove();
+            let goodKongCSS = document.createElement('link');
+            goodKongCSS.rel = 'stylesheet';
+            goodKongCSS.setAttribute('data-turbo-track', 'reload');
+            goodKongCSS.href = 'https://fancy2209.github.io/KOG/GamePage.css';
+            document.head.appendChild(goodKongCSS);
+        }
+        else if(n.href.search("application_merged")>-1 && n.getAttribute("data-turbo-track")=="reload"){
+            n.remove();
+            let goodKongCSS = document.createElement('link');
+            goodKongCSS.rel = 'stylesheet';
+            goodKongCSS.setAttribute('data-turbo-track', 'reload');
+            goodKongCSS.href = 'https://fancy2209.github.io/KOG/Main.css';
+            document.head.appendChild(goodKongCSS);
+        }
+        else if(n.href.search("application-")>-1 && n.getAttribute("data-turbo-track")=="reload"){
+            n.remove();
+        }
+    }
+}
+
+function ThingsToDoAtTheEnd(){
+    ReopenChat(50);
+};
+
 (function() {
     'use strict';
 
     // Find the Things
-
-    var goodKongCSS = document.createElement('link');
-    goodKongCSS.rel = 'stylesheet';
-    goodKongCSS.setAttribute('data-turbo-track', 'reload');
 
     const headerWrap = `
     <!--============ #header ============-->
@@ -2761,6 +2887,7 @@ kong_ads.displayAd("kong_home_bf_281x90_3");
                                 node.remove();
                                 TimeToLogin();
                                 fill_games_tab(50);
+                                replace_css();
                             }
                             else if(v1==1 && node.tagName=="K-NAVBAR"){
                                 node.remove();
@@ -2792,21 +2919,17 @@ kong_ads.displayAd("kong_home_bf_281x90_3");
                                 customFavicon.href = 'https://raw.githubusercontent.com/Fancy2209/Good-Old-Kongregate/main/Icon/kong.png'; // Replace with the URL of your custom favicon
                                 document.head.appendChild(customFavicon);
                             }
-                            else if(v5==0 && node.tagName=="LINK" && node.rel=="stylesheet" && node.href.search("application_merged")>-1 && node.getAttribute("data-turbo-track")=="reload"){
+                            else if(v5==0 && 0==1){
                                 v5=1;
-                                node.remove();
-                                goodKongCSS.href = 'https://fancy2209.github.io/KOG/Main.css';
-                                document.head.appendChild(goodKongCSS);
+                                // FREE
                             }
-                            else if(v6==0 && node.tagName=="LINK" && node.rel=="stylesheet" && node.href.search("gamepage_merged")>-1 && node.getAttribute("data-turbo-track")=="reload"){
+                            else if(v6==0 && 0==1){
                                 v6=1;
-                                node.remove();
-                                goodKongCSS.href = 'https://fancy2209.github.io/KOG/GamePage.css';
-                                document.head.appendChild(goodKongCSS);
+                                // FREE
                             }
-                            else if(v7==0 && node.tagName=="LINK" && node.rel=="stylesheet" && node.href.search("application-")>-1 && node.getAttribute("data-turbo-track")=="reload"){
+                            else if(v7==0 && 0==1){
                                 v7=1;
-                                node.remove();
+                                // FREE
                             }
                             else if(v8==0 && document.URL=="https://www.kongregate.com/" && node.tagName=="MAIN"){
                                 v8=1;
@@ -2835,110 +2958,6 @@ kong_ads.displayAd("kong_home_bf_281x90_3");
     var observer = new MutationObserver(callback);
     observer.observe(targetNode, config);
 
-    function TimeToLogin(){
-        function updatePlaylist() {
-            var e = active_user.playLatersCount();
-            $$(".play-laters-count-link").each(function (t) {
-                (t.title = e + " games in your playlist."), t.down(".play-laters-count").update(e);
-            });
-        }
-        function updateFavorites() {
-            var e = active_user.favoritesCount();
-            $$(".favorites-count-link").each(function (t) {
-                (t.title = e + " favorites."), t.down(".favorites-count").update(e);
-            });
-        }
-        function updateFriendInfo(e) {
-            e.select(".friends_online_count").each(function (e) {
-                e.update(active_user.friendsOnlineCount());
-            }),
-                active_user.friendsOnlineCount() > 0
-                ? e.select(".friends_online_link").each(function (e) {
-                e.title = active_user.friendsOnlineNames();
-            })
-            : e.select(".friends_online_link").each(function (e) {
-                e.title = "No friends online.";
-            });
-        }
-
-        var go=0, t = $("welcome");
-        if(typeof(active_user)!="undefined"){
-            if (active_user.isAuthenticated() && t!=null){ go=1; }
-        }
-        if(go){
-            var e = active_user.getAttributes();
-            updateFriendInfo(t), updatePlaylist(), updateFavorites();
-            var n = new Element("img").writeAttribute({ id: "welcome_box_small_user_avatar", src: e.avatar_url, title: e.username, name: "user_avatar", alt: "Avatar for " + e.username, height: 28, width: 28 });
-            t.down("span#small_avatar_placeholder").update(n),
-                t.select(".facebook_nav_item").each(function (e) { active_user.isFacebookConnected() && e.hide(); }),
-                $("mini-profile-level").writeAttribute({ class: "spritesite levelbug level_" + e.level, title: "Level " + e.level }),
-                active_user.populateUserSpecificLinks(t),
-                t.select(".username_holder").each(function (e) { e.update(active_user.username()); });
-            var a = active_user.unreadShoutsCount() + active_user.unreadWhispersCount() + active_user.unreadGameMessagesCount();
-            a > 0 &&
-                ($("profile_bar_messages").addClassName("alert_messages"),
-                 $("profile_control_unread_message_count").update(a),
-                 $("profile_control_unread_message_count").addClassName("mls has_messages"),
-                 $("my-messages-link").setAttribute("title", active_user.unreadShoutsCount() + " shouts, " + active_user.unreadWhispersCount() + " whispers"),
-                 0 !== active_user.unreadWhispersCount()
-                 ? $("my-messages-link").setAttribute("href", "/accounts/" + active_user.username() + "/private_messages")
-                 : 0 !== active_user.unreadGameMessagesCount() && $("my-messages-link").setAttribute("href", "/accounts/" + active_user.username() + "/game_messages")),
-                null !== active_user.chipsBalance() , ($("blocks_balance").update(active_user.chipsBalance()), $("blocks").show()),
-                $("guest_user_welcome_content").hide(),
-                $("nav_welcome_box").show();
-        }
-        else{ setTimeout(function(){ TimeToLogin(); },10000); }
-    }
-
-    function ReopenChat(A){ // Not in the mutation observer in case we ever do more than just making the tab reappear.
-        var go=0;
-        if(typeof(holodeck)!="undefined" && document.getElementById("chat_tab")!=null){
-            if(holodeck.ready){ go=1; }
-        }
-        if(go){ document.getElementById("chat_tab").style.display="";}
-        else if(A){ setTimeout(function(B){ ReopenChat(B); },1000, A-1); }
-    }
-
-    function fill_games_tab(A){
-        if(typeof(navigationData)!="undefined"){
-            if(navigationData.user.authenticated){
-                document.getElementById("GOK_recently_played").setAttribute("href",navigationData.user.recently_played_path);
-                var l=navigationData.games.recently_played.length;
-                if(l>0){
-                    document.getElementById("GOK_no_recently_played_game").style.display="none";
-                    document.getElementById("GOK_recently_played_1").style.display="";
-                    document.getElementById("GOK1_image").setAttribute("src",navigationData.games.recently_played[0].icon_path);
-                    document.getElementById("GOK1_name").innerText=navigationData.games.recently_played[0].title;
-                    document.getElementById("GOK_recently_played_1").setAttribute("href",navigationData.games.recently_played[0].game_path);
-                    for(let k=1; k<l; k++){
-                        document.getElementById("GOK_recently_played_"+(k+1)).setAttribute("href",navigationData.games.recently_played[k].game_path);
-                        document.getElementById("GOK_recently_played_"+(k+1)).innerText=navigationData.games.recently_played[k].title;
-                        document.getElementById("GOK_recently_played_"+(k+1)).style.display="";
-                    }
-                }
-                document.getElementById("GOK_playlist").setAttribute("href",navigationData.user.playlist_path);
-                var p=navigationData.games.playlist.length;
-                if(p>0){
-                    document.getElementById("GOK_no_playlist").style.display="none";
-                    document.getElementById("GOK_playlist_1").style.display="";
-                    document.getElementById("GOK_playlist_1_image").setAttribute("src",navigationData.games.playlist[0].icon_path);
-                    document.getElementById("GOK_playlist_1_name").innerText=navigationData.games.playlist[0].title;
-                    document.getElementById("GOK_playlist_1").setAttribute("href",navigationData.games.playlist[0].game_path);
-                    for(let k=1; k<p; k++){
-                        document.getElementById("GOK_playlist_"+(k+1)).setAttribute("href",navigationData.games.playlist[k].game_path);
-                        document.getElementById("GOK_playlist_"+(k+1)).innerText=navigationData.games.playlist[k].title;
-                        document.getElementById("GOK_playlist_"+(k+1)).style.display="";
-                    }
-                }
-                else{ document.getElementById("GOK_no_playlist_text").innerText="Add games to play them later."; }
-            }
-        }
-        else if(A){ setTimeout(function(B){ fill_games_tab(B); },1000, A-1); }
-    }
-
-    function ThingsToDoAtTheEnd(){
-        ReopenChat(50);
-    };
     ThingsToDoAtTheEnd();
 
 })();
